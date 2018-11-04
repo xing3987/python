e98b3492da4113adb6442540b5afe41ad04f6eb1
chenxing <xing3987@163.com>
2018-11-04 23:30:11 +0800

    2018-11-4

--- python1/py1/django/mysite/blog/views.py
+++ python1/py1/django/mysite/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import BlogArticles
+from django.shortcuts import render,get_object_or_404  #导入错误界面的函数
 # Create your views here.
 def blog_title(request):
     blogs=BlogArticles.objects.all()
     return render(request,'blog/titles.html',{'blogs':blogs}) #创建视图跳转连接，并把blogs绑定到request,key为“blogs”
+
+def blog_article(request,article_id):
+    #article=BlogArticles.objects.get(id=article_id)
+    article=get_object_or_404(BlogArticles,id=article_id)
+    pub=article.publish
+    return render(request,'blog/content.html',{"article":article,"publish":pub})
