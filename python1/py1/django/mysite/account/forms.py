d083ff0a4d396367ff047b98bad104a846869701
chenxing <xing3987@163.com>
2018-11-10 23:38:23 +0800

    2018/11/10

--- python1/py1/django/mysite/account/forms.py
+++ python1/py1/django/mysite/account/forms.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
 
-
-class LoginForm(forms.Form):
+class LoginForm(forms.Form):#form只能有实体定义的input框
     username=forms.CharField()
     password=forms.CharField(widget=forms.PasswordInput)
 
 
-class RegistrationForm(forms.Form):
+class RegistrationForm(forms.ModelForm):  #ModelForm可以有除实体外的input
     password=forms.CharField(widget=forms.PasswordInput,label="Password")
     password2=forms.CharField(widget=forms.PasswordInput,label="Confirm Password")
     
@@ -16,8 +16,14 @@ class RegistrationForm(forms.Form):
         model=User
         fields=("username","email")
         
-    def clean_password2(self):
+    def clean_password2(self): #需要验证使用clean_函数
         cd=self.cleaned_data
         if cd['password']!=cd['password2']:
             raise forms.ValidationError('passwords do not match')
         return cd['password']
+    
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model=UserProfile
+        fields=("birth","phone")
+        
--- python1/py1/django/mysite/account/views.py
+++ python1/py1/django/mysite/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm,RegistrationForm
+from .forms import LoginForm,RegistrationForm,UserProfileForm
 # Create your views here.
 def user_login(request):
     if request.method=='POST':
@@ -24,15 +24,20 @@ def user_login(request):
 def register(request):
     if request.method=="POST":
         user_form=RegistrationForm(request.POST)
+        userprofile_form=UserProfileForm(request.POST)
         if user_form.is_valid():
             new_user=user_form.save(commit=False)  #commit=false是表示数据不要存到数据库
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            new_profile=userprofile_form.save(commit=False)
+            new_profile.user=new_user  #指定表userprofile中关联的user
+            new_profile.save()
             return HttpResponse("successfully")
         else:
             return HttpResponse("sorry,your can not register.")
         
     else:
         user_form=RegistrationForm()
-        return render(request,'account/register.html',{'form':user_form})
+        userprofile_form=UserProfileForm()
+        return render(request,'account/register.html',{'form':user_form,'profile':userprofile_form})
     
