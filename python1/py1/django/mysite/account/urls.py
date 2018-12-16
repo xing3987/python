3605b45dbb4dfcc5568bc371c7ad1942e440eb31
chenxing <xing3987@163.com>
2018-12-17 00:02:10 +0800

    2018-12-17

--- python1/py1/django/mysite/account/urls.py
+++ python1/py1/django/mysite/account/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('loginout/',auth_views.LogoutView.as_view(template_name='registration/loginout.html'),name="user_loginout"),
     path('register/',views.register,name="user_register"),
     path('password-change/',auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name="password_change"),
-    #path('password-change-done/',auth_views.PasswordResetDoneView, name="password_change_done"),
+    path('password-change-done/',auth_views.PasswordResetDoneView, name="password_change_done"),
 ]
