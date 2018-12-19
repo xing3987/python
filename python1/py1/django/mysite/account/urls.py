d263fb27f5e27b1bb8680b2747810ea1287d1b40
chenxing <xing3987@163.com>
2018-12-20 00:00:05 +0800

    2018-12-20

--- python1/py1/django/mysite/account/urls.py
+++ python1/py1/django/mysite/account/urls.py
@@ -6,9 +6,9 @@ app_name = "account"
 
 urlpatterns = [
     #path('login/', views.user_login, name='user_login'),
-    path('login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name="user_login"),
-    path('loginout/',auth_views.LogoutView.as_view(template_name='registration/loginout.html'),name="user_loginout"),
+    path('login/',auth_views.LoginView.as_view(template_name='account/login.html'),name="user_login"),
+    path('loginout/',auth_views.LogoutView.as_view(template_name='account/loginout.html'),name="user_loginout"),
     path('register/',views.register,name="user_register"),
-    path('password-change/',auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name="password_change"),
-    path('password-change-done/',auth_views.PasswordResetDoneView, name="password_change_done"),
+    path('password-change/',auth_views.PasswordChangeView.as_view(template_name='account/password_change_form.html' ,success_url="/account/password-change-done/"), name="password_change"),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"), name='password_change_done'),
 ]
